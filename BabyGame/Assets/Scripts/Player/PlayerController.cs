59d7eab21c9e4412cbf113522db8e389ed7219f3
Tim Velling <tim.velling@hotmail.de>
2020-08-21 23:05:02 +0200

    Player Init
    Movement + Sprite

---
+++ BabyGame/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1f;
+
+    private Rigidbody2D m_rig;
+
+    [SerializeField] private Transform handTransform;
+    private Transform m_currentBaby;
+
+    private void Start()
+    {
+        m_rig = GetComponent<Rigidbody2D>();
+        m_rig.gravityScale = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextMove = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        m_rig.MovePosition(m_rig.position + nextMove * (speed * Time.fixedDeltaTime));
+    }
+
+    public void PickUp(Transform _other)
+    {
+        m_currentBaby = _other;
+        m_currentBaby.parent = handTransform;
+    }
+}
