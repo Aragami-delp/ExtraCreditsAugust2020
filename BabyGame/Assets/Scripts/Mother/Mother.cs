60b2c9b5ffc4e7f6294529c8eb9838dfd4d32b8d
Neoministein <acertartoni@gmail.com>
2020-08-22 02:19:59 +0200

    worked on adding and removing babies from the game scene

---
+++ BabyGame/Assets/Scripts/Mother/Mother.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mother
+{
+    private EMotherState state = EMotherState.GIVE_UP_LINE;
+    private Baby baby;
+
+    private float maxTimeUntilReturn = 20f;
+    private float timeLeftUntilReturn;
+
+    public Mother()
+    {
+        this.timeLeftUntilReturn = maxTimeUntilReturn;
+    }
+
+    public void removeTime(float timeToRemove)
+    {
+        timeLeftUntilReturn -= timeToRemove;
+
+        if (timeLeftUntilReturn < 0)
+        {
+            state = EMotherState.RETURN_LINE;
+        }
+    }
+
+    public void setBaby(Baby baby)
+    {
+        this.baby = baby;
+    }
+    
+    public Baby getBaby()
+    {
+        return baby;
+    }
+
+    public void setState(EMotherState state)
+    {
+        this.state = state;
+    }
+
+    public EMotherState getState()
+    {
+        return state;
+    }
+
+}
